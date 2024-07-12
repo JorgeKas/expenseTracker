e75282a65e3d5978e9c0ce43356ef6b7bbdace97
George <karalisg@gmail.com>
2024-07-13 00:11:29 +0300

    new upload on 130724

--- index.php
+++ index.php
@@ -2,4 +2,15 @@
 
 require "functions.php";
 
-$uri = $_SERVER['REQUEST_URI'];
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+
+$routes = [
+  '/' => 'controllers/index.php',
+  '/expenses' => 'controllers/expenses.php',
+  '/income' => 'controllers/income.php',
+  '/reports' => 'controllers/reports.php',
+];
+
+if (array_key_exists($uri, $routes)) {
+  require $routes[$uri];
+}
