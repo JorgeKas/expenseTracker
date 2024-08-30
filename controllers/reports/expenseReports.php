4a986be6a48458e85bcf53b4280e3c0b5e0d6583
George <karalisg@gmail.com>
2024-08-30 19:15:14 +0300

    fix dropdown

--- controllers/reports/expenseReports.php
+++ controllers/reports/expenseReports.php
@@ -1,5 +1,11 @@
 <?php
 
+$config = require('config.php');
+$db = new Database($config['database']);
+
 $heading = "Expenses Reporting";
+
+$transactions = $db->query("SELECT * FROM transactions")->fetchAll();
+
 
 require "views/expenseReports.view.php";
--- views/expenseReports.view.php
+++ views/expenseReports.view.php
@@ -4,6 +4,13 @@
 <main>
   <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
     <p>A page to list various types of expenses</p>
+      <?php foreach ($transactions as $transaction) : ?>
+        <li>
+          <a href="/transaction?id=<?= $transaction['id'] ?>" class="text-blue-500 underline:hover">
+            <?= $transaction['date'] . " " . $transaction['description'] . " " . $transaction['amount'] . "€" ?>
+          </a>
+        </li>
+      <?php endforeach; ?>
   </div>
 </main>
 <?php require('partials/footer.php') ?>
