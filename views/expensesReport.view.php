b5b15ddc56013c77ba11e7ae110812763b86d06b
George <karalisg@gmail.com>
2024-09-09 00:15:00 +0300

    A new commit to change date format on output - 080924

--- views/expensesReport.view.php
+++ views/expensesReport.view.php
@@ -5,9 +5,10 @@
   <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
     <p>A page to list various types of expenses</p>
       <?php foreach ($transactions as $transaction) : ?>
+        <?php $newDateFormat = date("d/m/Y", strtotime($transaction['date'])); ?>
         <li>
           <a href="/transaction?id=<?= $transaction['id'] ?>" class="text-blue-500 underline:hover">
-            <?= htmlspecialchars($transaction['date'] . " " . $transaction['description'] . " " . $transaction['amount'] . "€") ?>
+            <?= htmlspecialchars($newDateFormat . " | " . $transaction['description'] . " | " . $transaction['amount'] . " €") ?>
           </a>
         </li>
       <?php endforeach; ?>
